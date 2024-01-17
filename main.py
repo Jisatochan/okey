a453f8a413d973358683273efd285f766e1b3de6
Jisatochan <chan.love.pim1@gmail.com>
2024-01-17 15:52:04 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,3 @@
-import requests
-
-url = "https://www.example.com"
-response = requests.get(url)
-
-if response.status_code == 200:
-    print("Request successful!")
-    website_content = response.text
-    print(website_content)
-
-else:
-    print("Request failed with status code:", response.status_code)
+import streamlit as st
+st.header("จันทร์ บุญธรรม")
+st.write("64545202033-4")
